3b7801d32132d0163a82f7a46d2918660bea228e
fernandopalad@gmail.com <fernandopalad@gmail.com@fbfee344-e7e1-32e3-21f8-cc6a9d62636d>
2013-06-07 18:59:02 +0000

    CursoDAO, DisciplinaDAO

--- LocusView/src/entidades/Curso.java
+++ LocusView/src/entidades/Curso.java
@@ -4,70 +4,55 @@ import java.util.ArrayList;
 
 public class Curso {
 
-	//variaveis
-	private String nome;
-	private int id;
-	private ArrayList<Turma> turmas;
-	
-	//contrutores
-	public Curso(String nome, int id, ArrayList<Turma> turmas) {
-		super();
-		this.nome = nome;
-		this.id = id;
-		this.turmas = turmas;
-	}
+    //variaveis
+    private String nome;
+    private int id;
+    private ArrayList<Turma> turmas;
 
-	public Curso() {
-		super();
-	}
+    //contrutores
+    public Curso(String nome, int id, ArrayList<Turma> turmas) {
+        super();
+        this.nome = nome;
+        this.id = id;
+        this.turmas = turmas;
+    }
+
+    public Curso() {
+        super();
+    }
 
     @Override
     public String toString() {
         return "Curso{" + "nome=" + nome + ", id=" + id + ", turmas=" + turmas + '}';
     }
 
-	//codigos
-	
-	void adicionarTurma(Turma turma){
-	turmas.add(turma);
-	}
-	
-	
-	
-	//gets e sets
-	public String getNome() {
-		return nome;
-	}
+    //codigos
+    void adicionarTurma(Turma turma) {
+        turmas.add(turma);
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    //gets e sets
+    public String getNome() {
+        return nome;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public ArrayList<Turma> getTurmas() {
-		return turmas;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setTurmas(ArrayList<Turma> turmas) {
-		this.turmas = turmas;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    public ArrayList<Turma> getTurmas() {
+        return turmas;
+    }
+
+    public void setTurmas(ArrayList<Turma> turmas) {
+        this.turmas = turmas;
+    }
 }
---
+++ LocusView/src/model/DisciplinaDAO.java
@@ -0,0 +1,124 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import entidades.Curso;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author silvio
+ */
+public class DisciplinaDAO {
+    
+    /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import entidades.Curso;
+import entidades.Midia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author luiz_malaquias
+ */
+public class CursoDAO {
+   
+    public ArrayList<Curso> select() {
+
+        Connection connection = Conexao.getConexao();
+        try {
+
+            String sql = "SELECT * FROM curso";
+            PreparedStatement prest = connection.prepareStatement(sql);
+            ResultSet rs = prest.executeQuery();
+
+            ArrayList<Curso> listaClientes = new ArrayList<Curso>();
+            while (rs.next()) {
+                Curso c = new Curso();
+                int id = rs.getInt("idCurso");
+                String nome = rs.getString("nome");
+                c.setId(id);
+                c.setNome(nome);
+                listaClientes.add(c);
+            }
+
+            connection.close();
+            return listaClientes;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    public void insert(Curso curso) {
+        Connection connection = Conexao.getConexao();
+        try {
+            String sql = "INSERT INTO curso (nome) VALUES (?);";
+            PreparedStatement prest = connection.prepareStatement(sql);
+           
+            prest.setString(1,curso.getNome() );
+            
+
+            prest.execute();
+            connection.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void update(Curso cursoNovo, Curso cursoAntigo) {
+        Connection connection = Conexao.getConexao();
+        try {
+
+            String sql = "UPDATE curso SET nome = ? WHERE nome like ?";
+            //String sql = "UPDATE cliente SET nome = ? WHERE id = ?";
+            PreparedStatement prest = connection.prepareStatement(sql);
+            prest.setString(1, cursoNovo.getNome());
+            prest.setString(2, cursoAntigo.getNome());
+            //prest.setInt(2,clienteAntigo.getId());
+
+            prest.execute();
+            connection.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void delete(DisciplinaDAO disciplinaAntiga) {
+        Connection connection = Conexao.getConexao();
+        try {
+            String sql = "DELETE FROM curso WHERE nome = ?";
+            //String sql = "UPDATE cliente SET nome = ? WHERE id = ?";
+            PreparedStatement prest = connection.prepareStatement(sql);
+            prest.setString(1, disciplinaAntiga.getNome());
+
+            prest.execute();
+            connection.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    
+    
+    
+   
+    
+}
+
+    
+}
