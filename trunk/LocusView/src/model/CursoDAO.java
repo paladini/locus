1d4d97d728b1b4210012c2ea62f22a74fb0b82c3
fernandopalad@gmail.com <fernandopalad@gmail.com@fbfee344-e7e1-32e3-21f8-cc6a9d62636d>
2013-06-25 12:43:37 +0000

    PrimeiraEntrada3 pronta!

--- trunk/LocusView/src/model/CursoDAO.java
+++ trunk/LocusView/src/model/CursoDAO.java
@@ -11,22 +11,22 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Dictionary;
 
 /**
  *
  * @author luiz_malaquias
  */
-public class CursoDAO extends AbstractDAO{
-   
+public class CursoDAO extends AbstractDAO {
+
     /**
      * ALTERAR ESSE MÉTODO E DEIXÁ-LO FUNCIONAL!
-     * @param curso 
+     *
+     * @param curso
      */
-    
     /**
      * Faz consulta no banco de dados e retorna todos os cursos.
-     * @return 
+     *
+     * @return
      */
     public ArrayList<Curso> select() {
 
@@ -57,9 +57,10 @@ public class CursoDAO extends AbstractDAO{
 
     /**
      * Faz consulta no banco de dados e retorna apenas um curso com esse nome.
-     * @return 
+     *
+     * @return
      */
-    public Curso selectCurso(String nomeCurso){
+    public Curso selectCurso(String nomeCurso) {
         Connection connection = Conexao.getConexao();
         try {
 
@@ -70,14 +71,14 @@ public class CursoDAO extends AbstractDAO{
 
             // Cria uma nova disciplina
             Curso curso = new Curso();
-            
+
             // Pega o primeiro registro do retorno da consulta
             rs.next();
-            
+
             // Pega os dados desse registro e guarda em variáveis
             int id = rs.getInt("idCurso");
             String nome = rs.getString("nome");
-            
+
             // Seta os dados na disciplina criada
             curso.setId(id);
             curso.setNome(nome);
@@ -89,12 +90,13 @@ public class CursoDAO extends AbstractDAO{
         }
         return null;
     }
-    
-    
+
     /**
-     * Faz uma consulta no banco de dados pesquisando pelos termos digitados até o momento.
+     * Faz uma consulta no banco de dados pesquisando pelos termos digitados até
+     * o momento.
+     *
      * @param termos Termos digitados pelo usuário.
-     * @return 
+     * @return
      */
     public ArrayList<Curso> selectComTermos(String termos) {
 
@@ -123,7 +125,7 @@ public class CursoDAO extends AbstractDAO{
         }
         return null;
     }
-    
+
     public void insert(Curso curso) {
         String sql = "INSERT INTO curso (nome) VALUES (?);";
         ArrayList<Object> params = new ArrayList<Object>();
@@ -140,36 +142,139 @@ public class CursoDAO extends AbstractDAO{
     }
 
     public void delete(Curso cursoAntigo) {
+
+        // Chama um outro método para excluir as disciplinas associadas a esse curso.
+        this.deleteAssociacoes(cursoAntigo);
+
         String sql = "DELETE FROM curso WHERE nome = ?;";
         ArrayList<Object> params = new ArrayList<Object>();
         params.add(cursoAntigo.getNome());
         operacaoEscrita(sql, params);
     }
     
+    /*
+     * 
+     *    ======================================================================
+     * 
+     *                   Métodos de interação curso-disciplina
+     * 
+     *    ======================================================================
+     * 
+     */
+
     
-    public void insertCursoDisciplina(Curso curso) {
-        String sql = "INSERT INTO `locus`.`Curso_has_Disciplina` (`Curso_idCurso`, `Disciplina_idDisciplina`) VALUES (?, ?);";
-        
-        
-        for (Disciplina d : curso.getDisciplina() ) {
-            ArrayList<Object> params = new ArrayList<Object>();
-            params.add(curso.getId());
-            params.add(d.getId() );
-            operacaoEscrita(sql, params);
+    /**
+     * Retorna a lista de todas as disciplinas associadas à esse curso
+     *
+     * @param curso
+     */
+    public ArrayList<Disciplina> listaDisciplinasAssociadas(Curso curso) {
+        Connection connection = Conexao.getConexao();
+        try {
+
+            String sql = "select d.idDisciplina, d.nome from Curso_has_Disciplina as cd inner join disciplina as d on "
+                    + "d.idDisciplina = cd.Disciplina_idDisciplina where cd.Curso_idCurso = ?;";
+            PreparedStatement prest = connection.prepareStatement(sql);
+            prest.setInt(1, curso.getId());
+            ResultSet rs = prest.executeQuery();
+
+            ArrayList<Disciplina> listaDisciplinas = new ArrayList<Disciplina>();
+            while (rs.next()) {
+                Disciplina disciplina = new Disciplina();
+                int id = rs.getInt("idDisciplina");
+                String nome = rs.getString("nome");
+                disciplina.setId(id);
+                disciplina.setNome(nome);
+                listaDisciplinas.add(disciplina);
+            }
+
+            connection.close();
+            return listaDisciplinas;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
         }
+        return null;
     }
-    
+
+    /**
+     * Retorna a lista de todas as disciplinas não associadas à esse curso
+     *
+     * @param curso
+     */
+    public ArrayList<Disciplina> listaDisciplinasNaoAssociadas(Curso curso) {
+        Connection connection = Conexao.getConexao();
+        try {
+
+            String sql = "select d.idDisciplina, d.nome from disciplina d where d.idDisciplina not in "
+                    + "(select Disciplina_idDisciplina from Curso_has_Disciplina where Curso_idCurso = ?);";
+            PreparedStatement prest = connection.prepareStatement(sql);
+            prest.setInt(1, curso.getId());
+            ResultSet rs = prest.executeQuery();
+
+            ArrayList<Disciplina> listaDisciplinas = new ArrayList<Disciplina>();
+            while (rs.next()) {
+                Disciplina disciplina = new Disciplina();
+                int id = rs.getInt("idDisciplina");
+                String nome = rs.getString("nome");
+                disciplina.setId(id);
+                disciplina.setNome(nome);
+                listaDisciplinas.add(disciplina);
+            }
+
+            connection.close();
+            return listaDisciplinas;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Adiciona uma disciplina ao curso.
+     *
+     * @param curso
+     */
+    public void insertCursoDisciplina(int idCurso, int idDisciplina) {
+        String sql = "INSERT INTO `locus`.`Curso_has_Disciplina` (`Curso_idCurso`, `Disciplina_idDisciplina`) VALUES (?, ?);";
+        ArrayList<Object> params = new ArrayList<Object>();
+        params.add(idCurso);
+        params.add(idDisciplina);
+        operacaoEscrita(sql, params);
+    }
+
+    /**
+     * Deleta uma disciplina associada ao curso.
+     *
+     * @param idCurso
+     * @param idDisciplina
+     */
     public void deleteCursoDisciplina(int idCurso, int idDisciplina) {
         String sql = "delete from Curso_has_Disciplina where Curso_idCurso = ? and Disciplina_idDisciplina = ?;";
-        
-         ArrayList<Object> params = new ArrayList<Object>();
-         params.add( idCurso );
-         params.add( idDisciplina );
-         operacaoEscrita(sql, params);
+
+        ArrayList<Object> params = new ArrayList<Object>();
+        params.add(idCurso);
+        params.add(idDisciplina);
+        operacaoEscrita(sql, params);
     }
+
+    /*
+     * ============================================
+     * 
+     *                Métodos Privados
+     * 
+     * ============================================
+     */
     
-    
-    
-   
-    
+    /**
+     * Deleta todas as associações entre curso e disciplina. 
+     * (Necessário para poder excluir o curso sem deixar dependências).
+     *
+     * @param curso
+     */
+    private void deleteAssociacoes(Curso curso) {
+        String sql = "delete from Curso_has_Disciplina where Curso_idCurso = ?;";
+        ArrayList<Object> params = new ArrayList<Object>();
+        params.add(curso.getId());
+        operacaoEscrita(sql, params);
+    }
 }
