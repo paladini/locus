87cc42984a29cf14510c790bad3f20f869925cd2
lfmhgdb@gmail.com <lfmhgdb@gmail.com@fbfee344-e7e1-32e3-21f8-cc6a9d62636d>
2013-11-20 17:38:41 +0000

    xhtml ensalamento

--- TCC/src/mbeans/EnsalamentoMBean.java
+++ TCC/src/mbeans/EnsalamentoMBean.java
@@ -6,6 +6,10 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ValueChangeEvent;
 import org.primefaces.context.RequestContext;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleModel;
+
 import control.ControleDisciplina;
 import entidades.Disciplina;
 
@@ -14,11 +18,16 @@ import entidades.Disciplina;
 public class EnsalamentoMBean {
 
 
-	public EnsalamentoMBean() {
-		
-		
-		
-
-	}
+	private ScheduleModel eventModel;  
+  
+    public EnsalamentoMBean() {  
+        eventModel = new DefaultScheduleModel();  
+//        eventModel.addEvent(new DefaultScheduleEvent("Champions League Match", previousDay8Pm(), previousDay11Pm()));  
+//        eventModel.addEvent(new DefaultScheduleEvent("Birthday Party", today1Pm(), today6Pm()));  
+//        eventModel.addEvent(new DefaultScheduleEvent("Breakfast at Tiffanys", nextDay9Am(), nextDay11Am()));  
+//        eventModel.addEvent(new DefaultScheduleEvent("Plant the new garden stuff", theDayAfter3Pm(), fourDaysLater3pm()));  
+//        Date comeco = new Date()
+//        new DefaultScheduleEvent("Teste", dataComeco, dateFim)
+    } 
 
 }
